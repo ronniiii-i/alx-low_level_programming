1135c30d13684cbde1b3d5caabd833834ffbf528
ronniiii-i <ronniiii@yahoo.com>
2022-09-08 09:43:13 -0700

    printing without printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * Description: main - Print out a string to stdout.
+ * Return: 0 if successfull.
+ */
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
