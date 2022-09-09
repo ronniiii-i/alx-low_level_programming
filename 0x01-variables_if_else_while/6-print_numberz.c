a1158592026ce3514b736e74193d551e574ebe69
ronniiii-i <ronniiii@yahoo.com>
2022-09-09 10:16:02 -0700

    print with putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /**
- * Description: main - Print out a string to stdout.
- * Return: 0 if successfull.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
+	int d;
 
-	for (i = 0; i < 10; i++)
-		putchar(i);
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
 	putchar('\n');
 
 	return (0);
