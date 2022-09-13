8fcee95553b1a9c23f0f8517020c708e8f5d74c9
ronniiii-i <ronniiii@yahoo.com>
2022-09-13 15:25:08 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: char type letter
  * Return: 1 if lowercase, 0 if not lowercase
  */
-int _islower(int c)
+int _isalpha(int c)
 {
         if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
                 return (1);
