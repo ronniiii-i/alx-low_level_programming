d022e8331d99a2fe58e466c50fd01c0b61ec2daf
ronniiii-i <ronniiii@yahoo.com>
2022-09-13 10:32:58 -0700

    task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Description: main - Print out a string to stdout.
+ * Return: 0 if successfull.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
