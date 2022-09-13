08999f2e3e2bad742d0ace3c06d7375554563acd
ronniiii-i <ronniiii@yahoo.com>
2022-09-13 15:21:59 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+  GNU nano 4.8                             3-islower.c
+#include "main.h"
+/**
+ * _islower - Return 1 if letter is lowercase, 0 if not.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
+ */
+int _islower(int c)
+{
+        if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+                return (1);
+        else
+                return (0);
+}
