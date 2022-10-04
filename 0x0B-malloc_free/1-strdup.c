828c428e0619a62e498de996ae5b945480eeb169
ronniiii-i <ronniiii@yahoo.com>
2022-10-04 12:53:31 -0700

    task 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - copy the string given to a newly allocated space in memory
+ * @str: string to copy
+ * Return: returns a pointer to the copy, or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	char *s = str; /*to access the address of the string*/
+	unsigned int i = 1, j;/*i to get length of str and j for loop*/
+
+	if (s == NULL)
+		return (NULL);
+	while (*s != '\0')
+		i++;
+	char *ptr = (char *)malloc(i * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+	for (j = 0; j <= i; j++)
+		ptr[j] = s[j];
+	return (ptr);
+}
