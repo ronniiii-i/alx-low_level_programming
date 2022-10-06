88fbae38e45b24161c8f00b3decfb9e930d43a75
ronniiii-i <ronniiii@yahoo.com>
2022-10-06 06:31:14 -0700

    main.h and task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of variable
+ * Return: address of variable
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	return (ptr);
+}
