e8f0450ef7ac13204cd26a3cac1e9768becdd549
ronniiii-i <ronniiii@yahoo.com>
2022-10-11 15:58:10 -0700

    task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,4 +12,12 @@ struct dog
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+
 #endif
