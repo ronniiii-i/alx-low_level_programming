0d0856ae29d70047c63b86174b21355d02c88073
ronniiii-i <ronniiii@yahoo.com>
2022-10-11 15:17:46 -0700

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,5 @@
-#ifndef ABS
-#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+#ifndef _3_FUNCTION_LIKE_MACRO_H
+#define _3_FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 #endif
